08ba40f00b4e5acca49c75a393285d1969c35ca1
FuzzyGrim <stromed_chariotman@aleeas.com>
2025-06-16 21:22:03 +0200

    reduce size for notes fields in track form #573

--- src/app/forms.py
+++ src/app/forms.py
@@ -217,7 +217,7 @@ class MediaForm(forms.ModelForm):
             "start_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
             "end_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
             "notes": forms.Textarea(
-                attrs={"placeholder": "Add any notes or comments..."},
+                attrs={"placeholder": "Add any notes or comments...", "rows": "5"},
             ),
         }
 
